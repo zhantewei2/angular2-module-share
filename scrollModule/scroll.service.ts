b47e827cb1b25db68ce3340610dc15f17b9ff0b3
zhantewei <zhantewei@gmail.com>
2017-02-05 17:44:38 +0800

    Update scroll.service.ts

--- scrollModule/scroll.service.ts
+++ scrollModule/scroll.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Subject';
-import 'rxjs/add/observable/of';
 @Injectable()
 export class ScrollService{
 	scrollTop:Subject<number>=new Subject();
