2cd33fc7f3219c8c8ff8220ff0350bd7d4a77afe
zhantewei <zhantewei@gmail.com>
2017-02-24 17:36:33 +0800

    Add files via upload

--- scrollModule/scroll.directive.ts
+++ scrollModule/scroll.directive.ts
@@ -1,22 +1,24 @@
-import {Directive} from '@angular/core';
+import {Directive,Input} from '@angular/core';
 import {ScrollService} from './scroll.service';
 @Directive({
 	selector:'[ztwScrollBind]'
 })
 export class ScrollDirective{
+	@Input('ztwScrollBind')baseLine:number;
 	constructor(
 		private scrollService:ScrollService
 	){
-		window.onscroll=function(){
+		window.addEventListener('scroll',()=>{
 			let topNum=document.querySelector('body').scrollTop;
 			let nodes=scrollService.controlNodes;
 			if(nodes===[])return;
+			this.baseLine=this.baseLine||0;
 			nodes.forEach(node=>{
-				let top=scrollService.getAbsoluteTop(node);
+				let top=scrollService.getAbsoluteTop(node)-this.baseLine;
 				node.ztw_top=top;
 				node.ztw_bottom=top+node.offsetHeight;
 			})
 			scrollService.scrollTop.next(topNum);
-		}
+		});
 	}
 }
